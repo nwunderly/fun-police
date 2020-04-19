510cfa5924767ac23664d05b92f41ae112dd2af9
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-04-19 19:45:38 -0400

    regex testing framework

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,6 @@
 
+import aiohttp
+
 # custom imports
 from utils.helpers import setup_logger
 from bot.backend import Astley
@@ -16,6 +18,8 @@ class Rick(Astley):
     def __init__(self):
         super().__init__(command_prefix="rr!")
         self.scanners = list()
+        self.url_patterns = dict()
+        self.youtube_url_patterns = dict()
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -24,9 +28,7 @@ class Rick(Astley):
             await self.process_private_messages(message)
         else:
             await self.process_commands(message)
-            if message.channel.category.id != 577706266768703488 and message.channel.id != 671418412127355040:
-                return  # todo remove this later
-            await self.process_rick_rolls(message)
+            # await self.process_rick_rolls(message) todo: add this back
 
     async def process_private_messages(self, message):
         msg = f"Received private message from {message.author} ({message.author.id}) - {message.clean_content}"
@@ -35,18 +37,51 @@ class Rick(Astley):
 
     async def process_rick_rolls(self, message):
         logger.debug(f"Checking message {message.id}.")
-        results = dict()
-        for scanner in self.scanners:
-            result = await scanner.scan(message)
-            results[scanner.name] = result
-        if await self.process_results(results):
-            s = f"Url in the previous message triggered the following scanners:"
-            for key, value in results.items():
-                if isinstance(value, ScanResults) or value > 0:
-                    s += f"\n{key}: {value}"
-            await message.channel.send(s)
-        else:
-            logger.debug(f"Nothing of note found in message {message.id}.")
+        status = ""
+        identified_urls = await self.process_urls(message.content)
+        status += f"RESULTS FROM URL REGEX\n"
+        resolved_urls = await self.process_redirects(identified_urls)
+        status += f"RESULTS FROM REDIRECT CHASING\n"
+        identified_youtube = await self.process_youtube_urls(resolved_urls)
+        status += f"RESULTS FROM YOUTUBE REDIRECTS\n"
+
+        await message.channel.send(status)
+
+        # results = dict()
+        # for scanner in self.scanners:
+        #     result = await scanner.scan(message)
+        #     results[scanner.name] = result
+        # if await self.process_results(results):
+        #     s = f"Url in the previous message triggered the following scanners:"
+        #     for key, value in results.items():
+        #         if isinstance(value, ScanResults) or value > 0:
+        #             s += f"\n{key}: {value}"
+        #     await message.channel.send(s)
+        # else:
+        #     logger.debug(f"Nothing of note found in message {message.id}.")
+
+    async def process_urls(self, content):
+        matches = dict()
+        for name, pattern in self.url_patterns.items():
+            match_list = pattern.finditer(content)
+            matches[name] = [match.group(0) for match in match_list]
+        return matches
+
+    async def process_redirects(self, urls):
+        resolved = list()
+        async with aiohttp.ClientSession() as session:
+            for url in urls:
+                async with session.head(url, allow_redirects=True) as response:
+                    resolved.append(response.url.human_repr())
+        return resolved
+
+    async def process_youtube_urls(self, urls):
+        matches = dict()
+        for name, pattern in self.youtube_url_patterns.items():
+            match = pattern.fullmatch(urls)
+            if match:
+                logger.debug(f"Matched YouTube URL: {match.group(0)}")
+                matches[name] = match.group(0)
 
     async def process_results(self, results):
         """
