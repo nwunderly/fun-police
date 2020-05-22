0f098578614cd4e9258402b9756d7689aed44cc1
No-Jons <61525221+No-Jons@users.noreply.github.com>
2020-05-22 10:48:32 -0700

    added self.data_size

--- bot/frontend.py
+++ bot/frontend.py
@@ -23,6 +23,7 @@ class Rick(Astley):
         self.rickroll_pattern = rickroll_pattern
         self.session = aiohttp.ClientSession()
         self.base_url = "https://www.googleapis.com/youtube/v3/commentThreads?"
+        self.data_size = 100
 
     async def cleanup(self):
         await self.session.close()
