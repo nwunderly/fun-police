71289ffd4d018348522922e060f217a0e0615931
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-05-27 21:20:01 -0400

    remove "www." from URLs in redis cache
    edited redis-py script to iterate and delete/replace items

--- scripts/redis_testing.py
+++ scripts/redis_testing.py
@@ -2,15 +2,21 @@
 import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
+# r.set('www.youtube.com/watch?v=REWyCy_m39Q', 'bar')
 
 
-print(r.get('foo'))
+# print(r.get('youtube.com/watch?v=REWyCy_m39Q'))
 # b'bar'
 
 for key in r.scan_iter():
     print(key)
+    if key.decode().startswith('www.'):
+        print(' - deleting and replacing.')
+        value = r.get(key)
+        r.delete(key)
+        r.set(key.decode().replace('www.', ''), value)
 
+print('\n\n\n')
 
-
-
+for key in r.scan_iter():
+    print(key)
